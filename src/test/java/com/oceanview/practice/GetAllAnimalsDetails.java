3d2f4d8c7521fc2788cfb2a707ddd7179355152a
Kirankumar K <kirankhsn1@rediffmail.com>
2023-10-29 15:34:38 +0530

    first commit

---
+++ src/test/java/com/oceanview/practice/GetAllAnimalsDetails.java
@@ -0,0 +1,31 @@
+package com.oceanview.practice;
+
+import com.oceanview.listeners.MainTestListeners;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(MainTestListeners.class)
+public class GetAllAnimalsDetails {
+
+
+    public static RestAssured rest;
+
+    @Test
+    public static void getAllAnimalDetails(){
+        rest.baseURI = "https://oceanview-d5a2.restdb.io/rest";
+
+        Response response = rest.given()
+                .headers("x-apikey", "b3f79303352bccf01ba0a0aca60cf4e71123b")
+                .when()
+                .get("/animals");
+
+        String respBody = response.getBody().asPrettyString();
+        System.out.println(respBody);
+
+        response.then().statusCode(200);
+    }
+
+}
